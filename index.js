ab9e507d38e3a3023a6cae8195e4adfc90b9f02d
davcd <hello@davcd.com>
2021-01-24 21:50:49 +0100

    fix: retrieve cover-letter.md file when custom resume path is specified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const pug = require("pug");
 const helper = require('./assets/helper');
 const fs = require('fs')
+const path = require('path');
 
 const render = (resume) => pug.renderFile(__dirname + "/assets/template.pug", {
     resume: mergeResume(resume),
@@ -9,11 +10,22 @@ const render = (resume) => pug.renderFile(__dirname + "/assets/template.pug", {
 
 
 const mergeResume = (resume) => {
-    if (!resume.coverLetter && fs.existsSync(process.cwd() + '/cover-letter.md')) {
-        const coverLetter = fs.readFileSync(process.cwd() + '/cover-letter.md', 'utf-8')
+    const folder = resolverFolder()
+    if (!resume.coverLetter && fs.existsSync(path.join(folder, 'cover-letter.md'))) {
+        const coverLetter = fs.readFileSync(path.join(folder, 'cover-letter.md'), 'utf-8')
         resume.basics.coverLetter = coverLetter.toString()
     }
     return resume
+}
+
+const resolverFolder = () => {
+    const index = process.argv.indexOf('--resume')
+    if (index === -1) {
+        return process.cwd()
+    } else {
+        const resumePath = process.argv[index + 1]
+        return path.join(process.cwd(), path.dirname(resumePath))
+    }
 }
 
 const pdfRenderOptions = {}
