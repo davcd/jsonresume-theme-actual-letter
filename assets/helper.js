c16b47d3b7d33ea1a459ed06577397088612a1f3
davcd <hello@davcd.com>
2021-01-27 22:19:49 +0100

    refactor: added eslint standard

--- assets/helper.js
+++ assets/helper.js
@@ -1,38 +1,36 @@
-const md = require('markdown-it')();
-const iso = require('iso-3166-1');
-const moment = require('moment');
-
+const md = require('markdown-it')()
+const iso = require('iso-3166-1')
+const moment = require('moment')
 
 const mdToHtml = (string) => string ? md.render(string) : ''
 
 const calcLocation = (location) => {
-    let array = []
+  const array = []
 
-    if(!location){
-        return null
-    }
+  if (!location) {
+    return null
+  }
 
-    if (location.city) {
-        array.push(location.city)
-    }
+  if (location.city) {
+    array.push(location.city)
+  }
 
-    if (location.region) {
-        array.push(location.region)
-    }
+  if (location.region) {
+    array.push(location.region)
+  }
 
-    if (location.countryCode) {
-        const country = iso.whereAlpha2(location.countryCode)
-        array.push(country && array.length < 2 ? country.country : location.countryCode)
-    }
+  if (location.countryCode) {
+    const country = iso.whereAlpha2(location.countryCode)
+    array.push(country && array.length < 2 ? country.country : location.countryCode)
+  }
 
-
-    return array.length > 0 ? array.join(', ') : null
+  return array.length > 0 ? array.join(', ') : null
 }
 
 const getCurrentDate = () => moment().format('LL')
 
 module.exports = {
-    mdToHtml,
-    calcLocation,
-    getCurrentDate
+  mdToHtml,
+  calcLocation,
+  getCurrentDate
 }
--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const pug = require("pug");
-const helper = require('./assets/helper');
+const pug = require('pug')
+const helper = require('./assets/helper')
 const fs = require('fs')
-const path = require('path');
+const path = require('path')
 
-const render = (resume) => pug.renderFile(__dirname + "/assets/template.pug", {
-    resume: mergeResume(resume),
-    helper
+const render = (resume) => pug.renderFile(path.join(__dirname, 'assets', 'template.pug'), {
+  resume: mergeResume(resume),
+  helper
 })
 
-
 const mergeResume = (resume) => {
-    const folder = resolverFolder()
-    if (!resume.coverLetter && fs.existsSync(path.join(folder, 'cover-letter.md'))) {
-        const coverLetter = fs.readFileSync(path.join(folder, 'cover-letter.md'), 'utf-8')
-        resume.basics.coverLetter = coverLetter.toString()
-    }
-    return resume
+  const folder = resolverFolder()
+  if (!resume.coverLetter && fs.existsSync(path.join(folder, 'cover-letter.md'))) {
+    const coverLetter = fs.readFileSync(path.join(folder, 'cover-letter.md'), 'utf-8')
+    resume.basics.coverLetter = coverLetter.toString()
+  }
+  return resume
 }
 
 const resolverFolder = () => {
-    const index = process.argv.indexOf('--resume')
-    if (index === -1) {
-        return process.cwd()
-    } else {
-        const resumePath = process.argv[index + 1]
-        return path.join(process.cwd(), path.dirname(resumePath))
-    }
+  const index = process.argv.indexOf('--resume')
+  if (index === -1) {
+    return process.cwd()
+  } else {
+    const resumePath = process.argv[index + 1]
+    return path.join(process.cwd(), path.dirname(resumePath))
+  }
 }
 
 const pdfRenderOptions = {}
 
 module.exports = {
-    render,
-    pdfRenderOptions,
-    mergeResume
-};
+  render,
+  pdfRenderOptions,
+  mergeResume
+}
