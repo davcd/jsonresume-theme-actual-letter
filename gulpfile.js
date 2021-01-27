bfef0233fbe3cfbec501f45566dbc3f6ef934515
davcd <hello@davcd.com>
2021-01-27 22:42:11 +0100

    refactor: changed gulpfile.js code style

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest, watch, series} = require('gulp')
+const { src, dest, watch, series } = require('gulp')
 const pug = require('gulp-pug')
 const sass = require('gulp-sass')
 
@@ -8,36 +8,34 @@ const fs = require('fs')
 const helper = require('./assets/helper.js')
 const index = require('./index.js')
 
-function css() {
-    return src("./assets/styles.scss")
-        .pipe(sass())
-        .pipe(dest("./assets"))
+function css () {
+  return src('./assets/styles.scss')
+    .pipe(sass())
+    .pipe(dest('./assets'))
 }
 
+function html () {
+  const resume = JSON.parse(fs.readFileSync('./resume.json', 'utf-8'))
 
-function html() {
-    const resume = JSON.parse(fs.readFileSync('./resume.json', 'utf-8'))
-
-    return src('./assets/template.pug')
-        .pipe(pug({data: {resume: index.mergeResume(resume), helper}}))
-        .pipe(dest('./public'))
+  return src('./assets/template.pug')
+    .pipe(pug({ data: { resume: index.mergeResume(resume), helper } }))
+    .pipe(dest('./public'))
 }
 
-function serve() {
-    bs.init({
-        server: {
-            baseDir: './public',
-            index: 'template.html'
-        },
-        ui: false,
-        open: false
-    })
+function serve () {
+  bs.init({
+    server: {
+      baseDir: './public',
+      index: 'template.html'
+    },
+    ui: false,
+    open: false
+  })
 
-    watch('./assets/**/*.scss', series(css, html))
-    watch(['./assets/**/*.pug', './resume.json'], html)
-    bs.watch("./public/*.html").on("change", bs.reload);
+  watch('./assets/**/*.scss', series(css, html))
+  watch(['./assets/**/*.pug', './resume.json'], html)
+  bs.watch('./public/*.html').on('change', bs.reload)
 }
 
 exports.css = css
 exports.default = series(css, html, serve)
-
